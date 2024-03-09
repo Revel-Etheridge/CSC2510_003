936c72d708894edede8d89ebb1356ddb5c934d4e
Revel Anthony <93089702+Revel-Etheridge@users.noreply.github.com>
2024-03-09 11:45:33 -0600

    Lab08_V03
    Created checks and reading loops for file sent as argument 1.

--- class/classlist.sh
+++ class/classlist.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-echo $#
+
+#checking number of arguments sent
 if [ "$#" -ne 2 ]
 then
     echo "Incorrect number of arguments sent."
 else
     echo "Correct number of arguments received."
+fi
+
+#saving arguments
+argument1=$1
+argument2=$2
+
+#checking first argument is a file
+if [[ -f $argument1 ]]; then
+    echo "$1 is a file"
+    
+    #opening the file and searching for given username
+    #requires new line after last username
+    while IFS= read -r username
+        do
+            if [[ $argument2 = $username ]]; then
+                echo "Username found!"
+            fi
+        done < classlist.txt
+else
+    echo "$1 is not a file, please try again."
 fi
