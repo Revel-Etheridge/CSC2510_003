791b1eae2e5a0c42e17fd12e7d63a3dd19bc2f28
Revel Anthony <93089702+Revel-Etheridge@users.noreply.github.com>
2024-03-18 19:26:47 -0500

    Lab08_V05
    Added logic to search for and print the line number of given user name along with boolean value to keep track if the username was found.

--- class/classlist.sh
+++ class/classlist.sh
@@ -3,30 +3,37 @@
 #checking number of arguments sent
 if [ "$#" -ne 2 ]
 then
+    echo ""
     echo "Incorrect number of arguments sent."
 else
+    echo ""
     echo "Correct number of arguments received."
 fi
 
 #saving arguments
 argument1=$1
 argument2=$2
+#creating placeholder for username check
+userFound=0
 
 #checking first argument is a file
 if [[ -f $argument1 ]]; then
     echo "$1 is a file"
-
-    grep -n "$argument2" classlist.txt
-    
     #opening the file and searching for given username
     #requires new line after last username
     while IFS= read -r username
         do
             if [[ $argument2 = $username ]]; then
-                echo "Username found!"  
-                grep -n shell classlist.txt
+                echo "Username found! Found on line: "  
+                grep -n "$argument2" classlist.txt
+                (( userFound = 1 ))
             fi
-        done < classlist.txt
+    done < classlist.txt
+
+    #branching in case of failed search
+    if [[ $userFound = 0 ]]; then
+        echo "Username $argument2 was not found."
+    fi
 else
     echo "$1 is not a file, please try again."
 fi
