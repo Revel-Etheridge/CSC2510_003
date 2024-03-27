d842e6e52c2d34b4a2fe3d85a418640bbd38c9b5
reveletheridge <reveletheridge@csc-2510.us-east1-c.c.raetheridg42-csc2510-spring24.internal>
2024-03-27 02:30:35 +0000

    Print number of lines w/out substring

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 find /usr type -f | head -n 10
 find /usr -type f | head -n 10
 grep -in "model name" /proc/cpuinfo
+grep -v -c "sudo" /etc/group
--- utils.sh
+++ utils.sh
@@ -2,3 +2,4 @@ find /usr/bin -type f -name "c*"
 find /usr/bin -type l -name "*sh*"
 find /usr -type f | head -n 10
 grep -in "model name" /proc/cpuinfo
+grep -v -c "sudo" /etc/group
