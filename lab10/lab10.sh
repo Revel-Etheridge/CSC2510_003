c147c556b19c17f2d1e35246bcd3aeb0e0bbf8f2
Revel Anthony <93089702+Revel-Etheridge@users.noreply.github.com>
2024-03-30 19:34:26 -0500

    Created lab10.sh
    Checks command for given file and checks phonebook.dat by default

---
+++ lab10/lab10.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#determining which file will be checked
+if [ -z "$1" ]; then
+    checkFile="phonebook.dat"
+else 
+    checkFile="$1"
+fi
+
+#Problem 1
+
+
+#Problem 2
+
+
+#Problem 3
+
+
+#Problem 4
+
+
+#Problem 5
+
+
+#Problem 6
+
+
+#Problem 7
+
+
+#Problem 8
